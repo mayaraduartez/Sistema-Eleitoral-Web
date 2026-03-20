a67768e892339082e54be4bd090bba873e4d34ca
mayaraduartez <mayaraduart321@gmail.com>
2026-03-19 21:30:49 -0300

    banco conectado

---
+++ migrations/20260320002603-eleitores.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("eleitores", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      foto: Sequelize.STRING,
+      nome: {type: Sequelize.STRING, allowNull: false},
+      sobrenome: {type: Sequelize.STRING, allowNull: false},
+      data_nascimento: Sequelize.DATEONLY,
+      cpf: { type: Sequelize.STRING, unique:true },
+      email: { type: Sequelize.STRING, allowNull: false, unique:true },
+      senha: {type: Sequelize.STRING, allowNull: false},
+      titulo: Sequelize.STRING,
+      rua: Sequelize.STRING,
+      bairro: Sequelize.STRING,
+      cidade: Sequelize.STRING,
+      nro_endereco: Sequelize.STRING,
+      UF: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("eleitores");
+  },
+};
---
+++ models/Eleitor.js
@@ -0,0 +1,57 @@
+const { DataTypes } = require("sequelize");
+const sequelizeconnect = require("../config/connection");
+
+const Eleitor = sequelizeconnect.define(
+  "Eleitor",
+  {
+    nome: {
+      type: DataTypes.STRING,
+    },
+    sobrenome: {
+      type: DataTypes.STRING,
+    },
+    foto: {
+      type: DataTypes.STRING,
+    },
+    cpf: {
+        type: DataTypes.STRING,
+        unique: true,
+    },
+    data_nascimento: {
+      type: DataTypes.DATEONLY,
+    },
+    titulo: {
+        type: DataTypes.STRING,
+    },
+     rua: {
+  type: DataTypes.STRING,
+},
+ bairro: {
+  type: DataTypes.STRING,
+},
+cidade: {
+  type: DataTypes.STRING,
+},
+nro_endereco: {
+  type: DataTypes.STRING,
+},
+UF: {
+  type: DataTypes.STRING,
+},
+  email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "eleitores",
+  }
+);
+
+module.exports = Eleitor;
