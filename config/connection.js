0b88e43c03e63f52f275a2a93a66e6747b8bbcf4
Mayara Duarte <99914037+mayaraduartez@users.noreply.github.com>
2026-04-02 21:22:51 -0300

    Change database user from 'adminn' to 'postgres'

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const sequelizeconnect = new Sequelize(
     "db_sistemaeleitoral",
     "postgres",
-    "adminn",
+    "postgres",
     {
         host: "localhost",
         port: "5432",
